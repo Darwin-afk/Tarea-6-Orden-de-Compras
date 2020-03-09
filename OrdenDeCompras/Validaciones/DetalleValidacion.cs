a3c0914e9f59b2efde5c260da8275f78f242b30f
Darwin-afk <darwin_lantigua@ucne.edu.do>
2020-03-09 14:12:47 -0400

    Aplicando validacion al datagrid

---
+++ OrdenDeCompras/Validaciones/DetalleValidacion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Globalization;
+using OrdenDeCompras.Entidades;
+using System.Windows.Data;
+
+namespace OrdenDeCompras.Validaciones
+{
+    public class DetalleValidacion : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if(value!= null)
+            {
+                List<OrdenesDetalle> detalle = (List<OrdenesDetalle>)value;
+
+                if (detalle.Count>0)
+                {
+                    return ValidationResult.ValidResult;
+                }
+                return new ValidationResult(false, "Debes poner algun producto");
+            }
+            return new ValidationResult(false, "Debes poner algun producto");
+        }
+    }
+}
